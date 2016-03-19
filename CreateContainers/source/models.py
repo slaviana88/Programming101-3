09152048949a280a3da74d534e88e939dc8e607b
Slavyana Monkova <slaviana_m@mail.bg>
2016-03-19 12:59:00 +0200

    Delete create contrainers

--- CreateContainers/source/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-# Create your models here.
-
-class Container(models.Model):
-    name_container = models.CharField(max_length=50)
-    name_owner = models.CharField(max_length=50)
-    ssh_key = models.CharField(max_length=200)
